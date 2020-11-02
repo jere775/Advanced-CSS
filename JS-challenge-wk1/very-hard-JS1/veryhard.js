d0f5f889f9430f97e1079fa3fcda00699b78257e
jere775 <72086520+jere775@users.noreply.github.com>
2020-11-02 18:33:28 -0500

    Update veryhard.js

--- JS-challenge-wk1/very-hard-JS1/veryhard.js
+++ JS-challenge-wk1/very-hard-JS1/veryhard.js
@@ -1,29 +1,27 @@
 //////Very Hard
 
-var number = prompt("Enter A Number?");
-var operator = prompt("Enter An Operator?");
-var number2 = prompt("Enter A Second Number?");
-// var answer number = ;
+var _number ,_operator ,_numTwo ,_answer; 
+_number = parseInt(prompt("Enter A Number"));
+_operator = prompt("Enter An Operator");
+_numTwo = parseInt(prompt("Enter A Second Number"));
 
 
-switch 
-(operator)
- {
+switch (_operator) {
+    case "+":
+        _answer = _number + _numTwo;
+        console.log(_number + " + " + _numTwo + " = " + _answer);
+        break;
     case "-":
-        answer = number - number2;
-        console.log(number + " - " + number2 + " = " + answer);
+        _answer = _number - _numTwo;
+        console.log(_number + " - " + _numTwo + " = " + _answer);
         break;
     case "/":
-        answer = number / number2;
-        console.log(number + " / " + number2 + " = " + answer);
+        _answer = _number / _numTwo;
+        console.log(_number + " / " + _numTwo + " = " + _answer);
         break;
     case "*":
-        answer = number * number2;
-        console.log(number + " * " + number2 + " = " + answer);
-        break;
-    case "+":
-        answer = number + number2;
-        console.log(number + " + " + number2 + " = " + answer);
+        _answer = _number * _numTwo;
+        console.log(_number + " * " + _numTwo + " = " + _answer);
         break;
     default:
         console.log("Redo it");
