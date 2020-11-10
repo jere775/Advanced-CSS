6592f0a05ccffeb04e8abfb7f533bedf542cf89a
Judith Espinoza <judithespinoza@AdminisatorsMBP.lan1>
2020-11-09 19:06:35 -0500

    second update

--- JavaScript-wk2/easy-JS-wk2/easy.js
+++ JavaScript-wk2/easy-JS-wk2/easy.js
@@ -2,10 +2,18 @@
 
 // let students = [];
 // students [0]="Hello, my name is Judith Espinoza and I'm 21 years old.";
-// students [1]="Hi, my name is Bobby Bob and I'm 20 years old";
-// students [2]="Sup, my name is James Bond and I'm 18 years old";
+// students [1]="Hi, my name is Bobby Bob and I'm 20 years old.";
+// students [2]="Sup, my name is James Bond and I'm 18 years old.";
 // console.log(students);
 // console.log(students[2]);
 // console.log(students[1]);
 // console.log(students[0]);
 
+
+
+var firstName, lastName, age;
+firstName = ['Judith', 'Bobby', 'James'];
+lastName = ['Espinoza', 'Bob', 'Bond'];
+age = ['21','20','18'];
+var answer = (firstName, lastName, age) => `Hi, my name is ${firstName} ${lastName} and I'm ${age} years old.`;
+console.log(answer(firstName[1],lastName[1],age[1]));
